8363daae4297e59a0e3b0dd19e8c4a71fbcefa46
BOU-CIEL1 <jalilbougoffa@gmail.com>
2026-01-26 09:38:35 +0100

    Modification du PHP pour le connecter au C++ - Ne fonctionne pas pour le moment car mon programme C++ demande des entrées utilisateurs -> le serveur PHP tourne donc dans le vide

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 </head>
 <body>
     <h3>Temps</h3>
+    <p>Bonjour</p>
+    <?php
+    $output = shell_exec("./COVACIELCourse_Chronometrage/main");
+if ($output === null) {
+        echo "<p style='color:red;'>Erreur : Impossible d'exécuter le programme.</p>";
+    } else {
+        echo "<pre>Résultat programme : " . htmlspecialchars($output) . "</pre>";
+    }
+    ?>
     <h3>Infos Spectateurs</h3>
 </body>
 </html>
