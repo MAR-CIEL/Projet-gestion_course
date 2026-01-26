6e28d1c7b35a9267477a53e360369e4d63223f95
BOU-CIEL1 <jalilbougoffa@gmail.com>
2026-01-26 08:48:17 +0100

    Création de l'index.php pour l'affichage du temps et des infos spectateurs

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Affichage temps et infos spectateurs</title>
+</head>
+<body>
+    <h3>Temps</h3>
+    <h3>Infos Spectateurs</h3>
+</body>
+</html>
