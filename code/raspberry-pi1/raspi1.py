7b9d39670ef70d466e64325496524ee6bae27d11
alexandremarchant0@gmail.com <alexandremarchant0@gmail.com>
2026-04-21 12:33:35 +0200

    connexion en MQTT avec IHM 2

--- code/raspberry-pi1/raspi1.py
+++ code/raspberry-pi1/raspi1.py
@@ -1,39 +1,57 @@
+import paho.mqtt.client as mqtt
 from sense_hat import SenseHat
 from time import sleep
-import socket
+
+
+IP_du_docker = "172.17.50.149" 
+
 
 sense = SenseHat()
-
-r = (255, 0, 0)
-j = (255, 255, 0)
-b = (0, 0, 255)
-g = (0, 255, 0)
-
-rainbow = [r, r, r, r, r, r, r, r]
-copie   = [g, g, g, g, g, g, g, g]
-copie2  = [j, j, j, j, j, j, j, j]
+sense.clear()
 
 
-s = socket.socket()
-s.connect(("172.17.50.233", 5000))
-s.send(b"Hello Server")
-data = s.recv(1024)
-s.close()
+R = (255, 0, 0)
+G = (0, 255, 0)
 
-if data == b"ok":
+
+def animation_start():
     i = 0
     while i < 3 :
-        for y in range(8):
-            colour = rainbow[y]
-            for x in range(8):
-                sense.set_pixel(x, y, colour)
+        sense.clear(R)
         sleep(1)
         sense.clear()
         sleep(0.4)
         i = i+1
-    for b in range(8):
-        colour = copie[b]
-        for c in range(8):
-            sense.set_pixel(c, b, colour)
+    
+    sense.clear(G)
     sleep(5)
-    sense.clear()    
+    sense.clear()
+
+def on_message(client, userdata, message):
+    topic = message.topic
+    payload = str(message.payload.decode("utf-8"))
+    
+    print(f"Ordre reçu sur {topic} : {payload}")
+
+    if topic == "covaciel/START":
+        print("Lancement de l'animation de départ...")
+        animation_start()
+        
+    elif topic == "covaciel/STOP":
+        print("Arrêt : Extinction du Sense HAT")
+        sense.clear(R) 
+        sleep(5)
+        sense.clear()
+
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "Raspberry_Piste_Sense")
+client.on_message = on_message
+
+try:
+    print(f"Connexion au Broker {IP_du_docker}...")
+    client.connect(IP_du_docker, 1883)
+    client.subscribe("covaciel/#")
+    print("Prêt à recevoir les ordres de l'IHM.")
+    client.loop_forever()
+except Exception as e:
+    print(f"Erreur : {e}")
